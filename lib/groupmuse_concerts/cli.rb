cda16b5e6d372e9af409e3e319db9c89cd61ee3d
Ben Altman <altmanguitar@gmail.com>
2021-03-02 04:37:32 +0000

    cli goodbye method

--- lib/groupmuse_concerts/cli.rb
+++ lib/groupmuse_concerts/cli.rb
@@ -4,7 +4,19 @@ class GroupmuseConcerts::CLI
 
   def call
 #binding.pry
-    puts "Let's stream a concert!"
+    puts "Let's find a concert to stream!"
+    input = nil
+    while input != "exit"
+      get concerts
+      ask for search choice
+      display options
+      choose and diplay choice
+    end
+    goodbye
   end
 
+  def goodbye
+    puts "Enjoy your concert and thank you for supporting artists!"
+  end
+  
 end
