642a42c71fc4eb3b305d0bbfd44d366186f8c5d2
Ben Altman <altmanguitar@gmail.com>
2021-03-06 23:35:17 +0000

    concert attributes

--- lib/groupmuse_concerts/concert.rb
+++ lib/groupmuse_concerts/concert.rb
@@ -1,4 +1,14 @@
-class GroupmuseConcerts::concert
+class GroupmuseConcerts::Concert
+  attr_accessor :name, :ensemble, :day_time, :composers, :type, :url
+
+  @@all = []
+
+  def initialize
+
+
+  def self.all
+    @@all
+  end
 
   def self.this_week
     #returns concerts streaming this week
