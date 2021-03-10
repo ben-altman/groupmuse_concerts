8e4325ee4944f049bcb9c6dc795125737b9eb973
Ben Altman <altmanguitar@gmail.com>
2021-03-10 20:58:31 +0000

    stub cli methods

--- lib/groupmuse_concerts/cli.rb
+++ lib/groupmuse_concerts/cli.rb
@@ -3,35 +3,59 @@
 class GroupmuseConcerts::CLI
 
   def call
-    puts "Let's find a concert to stream!"
+    puts "Stuck at home, missing music? Let's find a concert to stream!"
+    find_concert
+  end
+
+  def find_concert
     get_concerts
+
     puts ""
     puts "Enter 1 to search for a composer"
     puts "Enter 2 to search for an instrument or ensemble"
     puts "Enter 3 to see a list of all the concerts"
     puts "Type 'exit' if you are finished"
+
     input = gets.strip
-    if input == 3
+
+    case input
+    when input == 1
+      search_composer
+    when input == 2
+      search_instrument
+    when input == 3
       print_all_concerts
+    when input == "exit"
+      goodbye
+    else
+      puts "I do not understand your choice. Type exit or try again."
     end
-    puts "pick a show!"
-    input = gets.strip
-    concert = GroupmuseConcerts::Concert.all[input.to_i-1]
 
-    print_concert(concert)
+    # puts "pick a show!"
+    # input = gets.strip
+    # concert = GroupmuseConcerts::Concert.all[input.to_i-1]
+    #
+    # print_concert(concert)
 
-    # while input != "exit"
-      # get concerts
-      # ask for search choice
-      # display options
-      # choose and diplay choice
-    # end
-    # goodbye
+  end
+  # while input != "exit"
+    # get concerts
+    # ask for search choice
+    # display options
+    # choose and diplay choice
+  # end
+  # goodbye
+
+  def search_composer
+
+  end
+
+  def search_instrument
+
   end
 
   def get_concerts
     a = GroupmuseConcerts::Scraper.new
-#binding.pry
     a.scrape_concerts
   end
 
@@ -56,4 +80,5 @@ class GroupmuseConcerts::CLI
   def goodbye
     puts "Enjoy your concert and thank you for supporting artists!"
   end
+  
 end
